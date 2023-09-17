72de767da9f5418e83288a5b4a31a36b064526cc
EdisontitoRamirez <edisonsanramirez@gmail.com>
2023-09-17 18:56:51 -0500

    actualizacion

--- classimax-main/theme/about-us.php
+++ classimax-main/theme/about-us.php
@@ -80,23 +80,11 @@
       </div>
       <div class="col-lg-6 pt-5 pt-lg-0" >
         <div class="about-content">
-<<<<<<< HEAD
-          <h3 class="font-weight-bold">Equipo YLBB</h3>
-          <p>El equipo de YLBB esta conformado por estudiantes del SENA que cursan la carrera de analisis y desarrollo de 
-            sotfware en el centro de diseño y metrologia distrito capital. Los miembros del grupo son: Anderson Tovar, Juan pinilla,
-            Juan alvis, Samuel Diaz, Simon Sanchez y Edison Ramirez.
-          </p>
-          <h3 class="font-weight-bold">Como podemos ayudarle</h3>
-          <p>Nuestro grupo creo esta pagina con fin de ayudar a las pymes de Bogota D.C al poderles ofrecer esta pagina como metodo
-            de publicidad y venta de sus productos y servicios.
-          </p>
-=======
           <h3 class="font-weight-bold" id="h3_us">Introduccion</h3>
           <p>Detrás de cada línea de código, cada diseño cuidadosamente elaborado y cada función innovadora, se encuentra nuestro dedicado equipo de desarrollo en Your Little Big Business. Somos un grupo apasionado de profesionales creativos y amantes de la tecnología que se unen con un propósito: hacer que tu experiencia en nuestra plataforma sea excepcional. </p>
           <br>
           <br>
           
->>>>>>> d1f62d5cfa4e2d14ae3bd63f13e2546151b9a78a
         </div>
       </div>
     </div>
@@ -125,11 +113,7 @@
           <img class="card-img-top personas" src="images/team/team2.jpg" class="img-fluid w-100" alt="Card image cap">
           <div class="card-body bg-gray text-center">
             <h5 class="card-title">Juan Alvis</h5>
-<<<<<<< HEAD
-            <p class="card-text">Desarrolador</p>
-=======
             <p class="card-text">Fundador/desarollador</p>
->>>>>>> d1f62d5cfa4e2d14ae3bd63f13e2546151b9a78a
           </div>
         </div>
       </div>
@@ -137,13 +121,8 @@
         <div class="card my-3 my-lg-0">
           <img class="card-img-top personas" src="images/admins/samuel.jpeg" class="img-fluid w-100" alt="Card image cap">
           <div class="card-body bg-gray text-center">
-<<<<<<< HEAD
-            <h5 class="card-title">Anderson tovar</h5>
-            <p class="card-text">Desarrollador</p>
-=======
             <h5 class="card-title">Samuel Diaz</h5>
             <p class="card-text">Fundador/desarollador</p>
->>>>>>> d1f62d5cfa4e2d14ae3bd63f13e2546151b9a78a
           </div>
         </div>
       </div>
@@ -151,10 +130,6 @@
         <div class="card my-3 my-lg-0">
           <img class="card-img-top personas" src="images/admins/simon.jpg" class="img-fluid w-100" alt="Card image cap">
           <div class="card-body bg-gray text-center">
-<<<<<<< HEAD
-            <h5 class="card-title">Samuel Diaz </h5>
-            <p class="card-text">Desarrollador</p>
-=======
             <h5 class="card-title">Simon Peralta</h5>
             <p class="card-text">Fundador/desarollador</p>
           </div>
@@ -166,7 +141,6 @@
           <div class="card-body bg-gray text-center">
             <h5 class="card-title">Edison Ramirez</h5>
             <p class="card-text">Fundador/desarollador</p>
->>>>>>> d1f62d5cfa4e2d14ae3bd63f13e2546151b9a78a
           </div>
         </div>
       </div>
@@ -181,22 +155,14 @@
         <div class="counter-content text-center bg-light py-4 rounded">
           <i class="fa fa-smile-o d-block"></i>
           <span class="counter my-2 d-block" data-count="2314">0</span>
-<<<<<<< HEAD
-          <h5>Clientes felices</h5>
-=======
           <h5>Usuarios felices</h5>
->>>>>>> d1f62d5cfa4e2d14ae3bd63f13e2546151b9a78a
           </script>
         </div>
       </div>
       <div class="col-lg-3 col-sm-6 my-lg-0 my-3">
         <div class="counter-content text-center bg-light py-4 rounded">
           <i class="fa fa-user-o d-block"></i>
-<<<<<<< HEAD
-          <span class="counter my-2 d-block" data-count="5">0</span>
-=======
           <span class="counter my-2 d-block" data-count="1013">0</span>
->>>>>>> d1f62d5cfa4e2d14ae3bd63f13e2546151b9a78a
           <h5>Miembros activos</h5>
         </div>
       </div>
@@ -204,22 +170,14 @@
         <div class="counter-content text-center bg-light py-4 rounded">
           <i class="fa fa-bookmark-o d-block"></i>
           <span class="counter my-2 d-block" data-count="2413">0</span>
-<<<<<<< HEAD
-          <h5>Pymes</h5>
-=======
           <h5>Productos activos</h5>
->>>>>>> d1f62d5cfa4e2d14ae3bd63f13e2546151b9a78a
         </div>
       </div>
       <div class="col-lg-3 col-sm-6 my-lg-0 my-3">
         <div class="counter-content text-center bg-light py-4 rounded">
           <i class="fa fa-smile-o d-block"></i>
           <span class="counter my-2 d-block" data-count="200">0</span>
-<<<<<<< HEAD
-          <h5>Valoraciones</h5>
-=======
           <h5>Servicios activos</h5>
->>>>>>> d1f62d5cfa4e2d14ae3bd63f13e2546151b9a78a
         </div>
       </div>
     </div>

ee467f8f00200ae35b89ca31c2bbcc79c3ab9b1c
ander <andercmdsena@gmail.com>
2023-11-16 11:59:35 -0500

    boton de eliminar

--- classimax-main/controller/mostrarCalificacionProductos.php
+++ classimax-main/controller/mostrarCalificacionProductos.php
@@ -1,6 +1,7 @@
 <?php
 
 function cargarCalificacion($id){
+    $usuario = $_SESSION['id'];
     $objConsulta = new Consultas();
     $result = $objConsulta->mostrarCalificacion($id);
 
@@ -9,25 +10,25 @@ function cargarCalificacion($id){
     } else {
         $comentariosMostrados = 0;
 
-        echo '<div style="max-height: 500px; overflow-y: auto;">';
+        echo '<div style="max-height: 500px; overflow-y: auto; position: relative; margin: 50px;">';
 
         foreach ($result as $f) {
             // Aplica estilos al div contenedor
-            echo '<div style="text-align: left; padding: 10px; margin: 20px 20px 20px 180px; display: flex; align-items: center;">';
+            echo '<div style="text-align: left; padding: 20px; margin: 20px 20px 20px 180px; display: flex; align-items: center; position: relative;">';
 
             // Muestra la foto del usuario o administrador
             if (isset($f['Nombre'])) {
-                echo '<img src="' . $f['foto_usuario'] . '" alt="Foto user" style="width: 75px; height: 75px; border-radius: 50%; margin-right: 10px;">';
+                echo '<img src="' . $f['foto_usuario'] . '" alt="Foto user" style="width: 75px; height: 75px; border-radius: 50%; margin-right: 20px;">';
             } elseif (isset($f['Nombres'])) {
-                echo '<img src="' . $f['foto_administrador'] . '" alt="Foto admin" style="width: 75px; height: 75px; border-radius: 50%; margin-right: 10px;">';
+                echo '<img src="' . $f['foto_administrador'] . '" alt="Foto admin" style="width: 75px; height: 75px; border-radius: 50%; margin-right: 20px;">';
             }
 
             // Muestra el nombre, la calificación y el comentario
             echo '<div>';
             if (isset($f['Nombre'])) {
-                echo '<p style="font-size: 16px; font-weight: bold;">' . $f['Nombre'] . ' ';
+                echo '<p style="font-size: 18px; font-weight: bold; margin-bottom: 10px;">' . $f['Nombre'] . ' ';
             } elseif (isset($f['Nombres'])) {
-                echo '<p style="font-size: 16px; font-weight: bold;">' . $f['Nombres'] . ' ';
+                echo '<p style="font-size: 18px; font-weight: bold; margin-bottom: 10px;">' . $f['Nombres'] . ' ';
             }
 
             // Muestra la calificación
@@ -37,8 +38,14 @@ function cargarCalificacion($id){
 
             // Muestra el comentario
             echo '</p>';
-            echo '<p style="font-size: 20px;">' . $f['comentarios'] . '</p>';
+            echo '<p style="font-size: 20px; margin-bottom: 20px;">' . $f['comentarios'] . '</p>';
             echo '</div>';
+
+            // Agrega el botón de eliminar con estilos
+            if($usuario = $f['id_usuario']){
+                echo '<button onclick="eliminarComentario(' . $f['id'] . ')" style="position: absolute; top: 80px; left: 115px; margin: 10px 0; padding: 10px; font-weight: bold; background-color: #d9534f; color: white; border: none; cursor: pointer; transition: opacity 0.3s;" onmouseover="this.style.opacity=0.8" onmouseout="this.style.opacity=1">Eliminar</button>';
+
+            }
 
             echo '</div>';
 
@@ -48,5 +55,8 @@ function cargarCalificacion($id){
         echo '</div>';
     }
 }
+
+
+
 ?>
 
